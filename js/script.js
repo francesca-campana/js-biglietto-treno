6b8d4633da0a9716cf26fbb2daf4a58c331ebcf4
francesca-campana <franx3rox@gmail.com>
2020-05-20 16:52:59 +0200

    full

--- js/script.js
+++ js/script.js
@@ -2,17 +2,24 @@ var eta = parseInt(prompt('Quanti anni hai?'));
 console.log(eta);
 var km = parseInt(prompt('Quanti km dovrai percorrere?'));
 console.log(km);
-//var euro = 1 * 0.21;
-//console.log(euro);
+
 var prezzoKm = Math.floor(km * 0.21);
-var percentuale = prezzoKm * 20 / 100;
-var prezzoScontoYoung = prezzoKm - percentuale;
+
+//Variabile prezzo sconto young
+var percentualeYoung = prezzoKm * 20 / 100;
+var prezzoScontoYoung = prezzoKm - percentualeYoung;
+
+//Variabile prezzo sconto old
+var percentualeOld = prezzoKm * 40 / 100;
+var prezzoScontoOld = prezzoKm - percentualeOld;
 
 if (eta <= 20) {
-document.getElementById("young").innerHTML = "Il tuo biglietto costa " + prezzoScontoYoung;
-
+document.getElementById("young").innerHTML = "Il tuo biglietto costa " + prezzoScontoYoung+ "€";
 
 }
+else if (eta >= 65 ) {
+document.getElementById("young").innerHTML = "Il tuo biglietto costa " + prezzoScontoOld+ "€";
+}
 else {
-  document.getElementById("young").innerHTML = "Il tuo biglietto costa " + prezzoKm;
+  document.getElementById("young").innerHTML = "Il tuo biglietto costa " + prezzoKm + "€";
 }
