1237ce862ceeb12ee832d37004585c11f88154b3
francesca-campana <franx3rox@gmail.com>
2020-05-20 19:11:32 +0200

    done

--- js/script.js
+++ js/script.js
@@ -14,13 +14,19 @@ var percentualeOld = prezzoKm * 40 / 100;
 var prezzoScontoOld = prezzoKm - percentualeOld;
 
 
-if (eta < 18) {
-document.getElementById("msg-prezzo").innerHTML = "Ciao, hai meno di 18 anni, stai usufruendo dello sconto young, così il tuo biglietto costa " + prezzoScontoYoung+ "€, " + "invece di " + prezzoKm + "€";
+if ((eta = isNaN()) && (km = isNaN())) {
+  alert("Ops, i dati inseriti non sono corretti");
 
-}
-else if (eta > 65 ) {
-document.getElementById("msg-prezzo").innerHTML = "Ciao, hai più di 65 anni, stai usufruendo dello sconto old, così il tuo biglietto costa " + prezzoScontoOld + "€, " + "invece di " + prezzoKm + "€";
-}
-else {
+
+} else {
+  if (eta < 18) {
+  document.getElementById("msg-prezzo").innerHTML = "Ciao, hai meno di 18 anni, stai usufruendo dello sconto young, così il tuo biglietto costa " + prezzoScontoYoung+ "€, " + "invece di " + prezzoKm + "€";
+
+  }
+  else if (eta > 65 ) {
+  document.getElementById("msg-prezzo").innerHTML = "Ciao, hai più di 65 anni, stai usufruendo dello sconto old, così il tuo biglietto costa " + prezzoScontoOld + "€, " + "invece di " + prezzoKm + "€";
+  }
+  else {
   document.getElementById("msg-prezzo").innerHTML = "Ciao, Il tuo biglietto costa " + prezzoKm + "€";
+  }
 }
